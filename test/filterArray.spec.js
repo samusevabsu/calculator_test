cf8b7a49eae989dbf151fbab69a11f95e8c675ff
Ekaterina Samuseva <ekaterina.samuseva@fivejars.com>
2022-04-01 17:56:36 +0300

    uncomment filterArray.spec.js

--- test/filterArray.spec.js
+++ test/filterArray.spec.js
@@ -1,19 +1,18 @@
-// const { expect } = require('chai');
-// const Lesson8 = require('../app/calculator');
+const { expect } = require('chai');
+const Lesson8 = require('../app/calculator');
 
-// describe('filterArray test', function() {
-//     let calc;
-//     beforeEach(function() {
-//         calc = new Lesson8();
-//     })
-//     afterEach(function() {
-//         calc = null;
-//     })
+describe('filterArray test', function() {
+    let calc;
+    beforeEach(function() {
+        calc = new Lesson8();
+    })
+    afterEach(function() {
+        calc = null;
+    })
     
-//     it('Array is returned', function(){
-//         let a = calc.getArray()();
-//         let b = calc.filterArray(a);
-//         console.log(b);
-//         expect(b).to.be.a('array');  
-//     }) 
-// })
+    it('Array is returned', function(){
+        let a = calc.getArray()();
+        let b = calc.filterArray(a);
+        expect(b).to.be.a('array');  
+    }) 
+})
